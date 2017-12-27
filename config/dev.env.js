193bcc0b14a8931584408ef8a71cdaabc90798d3
mark <454085441@qq.com>
2017-12-27 17:23:51 +0800

    first commit

---
+++ config/dev.env.js
@@ -0,0 +1,10 @@
+'use strict'
+const merge = require('webpack-merge')
+const prodEnv = require('./prod.env')
+
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"development"',
+  BASE_API: '"/dev"',
+  CLIENT_ID: '"2"',
+  CLIENT_SECRET: '"R0HrMSExcSMSlnVcPBptxNCwn6ImMqgT24EIzraD"'
+})
