66a05176cb7637e09ab2d16ff3f4267f9f1c748e
mark <454085441@qq.com>
2018-01-09 17:09:14 +0800

    上传图片

--- src/views/image/index.vue
+++ src/views/image/index.vue
@@ -1,19 +1,25 @@
 <template>
   <div class="app-container">
-    <el-upload
-        class="upload-demo"
-        drag
-        action="https://jsonplaceholder.typicode.com/posts/"
-        multiple>
-        <i class="el-icon-upload"></i>
-        <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
-        <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
-    </el-upload>
+    <updload :imageUrl.sync="imageUrl"></updload>
   </div>
 </template>
 
 <script>
+import updload from '@/components/Upload'
 export default {
-  name: 'image'
+  name: 'image_index',
+  data() {
+    return {
+      imageUrl: ''
+    }
+  },
+  watch: {
+    imageUrl: function(val) {
+      console.log(val)
+    }
+  },
+  components: {
+    updload
+  }
 }
 </script>
